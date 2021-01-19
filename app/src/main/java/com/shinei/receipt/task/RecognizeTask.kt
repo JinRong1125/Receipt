32a5519995fca2a14f4ae2bd2b8b62f22effeafa
JinRong1125 <w842001@gmail.com>
2021-01-19 22:50:47 +0900

    support multiple lines.

--- app/src/main/java/com/shinei/receipt/task/RecognizeTask.kt
+++ app/src/main/java/com/shinei/receipt/task/RecognizeTask.kt
@@ -2,6 +2,7 @@ package com.shinei.receipt.task
 
 import android.content.Context
 import android.graphics.Bitmap
+import androidx.core.util.forEach
 import com.google.android.gms.vision.Frame
 import com.google.android.gms.vision.text.TextRecognizer
 import com.shinei.receipt.entity.Response
@@ -9,12 +10,12 @@ import com.shinei.receipt.entity.Response
 class RecognizeTask {
 
     operator fun invoke(context: Context, bitmap: Bitmap): Response<String> {
-        val stringBuilder = StringBuilder()
         val textBlocks = TextRecognizer.Builder(context).build()
             .detect(Frame.Builder().setBitmap(bitmap).build())
-        for (i in 0 until textBlocks.size()) {
-            stringBuilder.append(textBlocks[textBlocks.keyAt(i)].value)
+        val texts = ArrayList<String>()
+        textBlocks.forEach { key, _ ->
+            texts.add(textBlocks[key].value)
         }
-        return Response.Success(stringBuilder.toString())
+        return Response.Success(texts.joinToString(separator = ","))
     }
 }
